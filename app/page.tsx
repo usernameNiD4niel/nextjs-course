8913ebb824a66c49b2145f8478dc0df822b8eab2
Daniel Rey <reyvillanuevadaniel@gmail.com>
2024-05-11 15:59:15 +0800

    day 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
@@ -28,6 +29,8 @@ export default function Page() {
           {/* Add Hero Images Here */}
         </div>
       </div>
+      <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+      <div className={styles.shape} />
     </main>
   );
 }
